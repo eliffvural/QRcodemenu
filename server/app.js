7905f7049d8bc93a988a1f138002b3bcbcb98936
Elif Vural <elifvural019@gmail.com>
2025-07-18 16:49:31 +0300

    mongodb entegrated

--- server/app.js
+++ server/app.js
@@ -1,11 +1,13 @@
 const express = require('express');       // Express framework'ünü projeye dahil ettik
 const cors = require('cors');             // CORS ayarlarını aktif ettik (frontend'e izin ver)
 const mongoose = require('mongoose');     // MongoDB bağlantısı için mongoose kullanacağız
-const dotenv = require('dotenv');         // .env dosyasından ortam değişkenlerini alır
+const dotenv = require('dotenv');  
+const connectDB = require('./config/db')      // .env dosyasından ortam değişkenlerini alır
 
 dotenv.config();                          // .env dosyasını aktif ettik
 
-const app = express();                    // Express uygulamasını başlattık
+const app = express();
+connectDB();                    // Express uygulamasını başlattık
 app.use(cors());                          // CORS’u aktif ettik
 app.use(express.json());                  // JSON body parse etmeyi sağladık
 
